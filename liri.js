ac979293cb18908de596f5c226ea49e9b0ceabf6
Erin <eringlabe@gmail.com>
2017-03-30 18:00:34 -0700

    command skeletons

--- liri.js
+++ liri.js
@@ -4,11 +4,25 @@ var request = require("request");
 
 var keys = require("./keys.js");
 
+var command = process.argv.slice(2);
+
 
 // `my-tweets`
+if (command == "my-tweets") {
+	console.log("tweet!");
+}
 
 // `spotify-this-song`
+if (command == "spotify-this-song") {
+	console.log("song!");
+}
 
 // `movie-this`
+if (command == "movie-this") {
+	console.log("movie!");
+}
 
 // `do-what-it-says`
+if (command == "do-what-it-says") {
+	console.log("as you wish!");
+}
