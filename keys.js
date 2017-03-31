e9a22c80e3c82d017cc731e33a68fb1a9cc3bdae
Erin <eringlabe@gmail.com>
2017-03-30 17:43:19 -0700

    keys.js

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '2xi8HdvtXr6SIIgPCe9IHnxKu',
+  consumer_secret: 'rduLVduv0Hz2qHkj6XImTJrAjRaN1MH9RDEZ0Q4z8mZxMcXEpk',
+  access_token_key: '2416206979-3G6Uog0OkAqNQWwmw99Q3llxZtB3rRihmtEifgu',
+  access_token_secret: 'sQ1fGMsIyYAMVKMWYDMtta0Ytwy0YXfle6LPI1VbzbFO8',
+}
