646754f1c6a62a425d941bfa42c469703a2642c9
Erin <eringlabe@gmail.com>
2017-03-30 17:50:52 -0700

    require

---
+++ liri.js
@@ -0,0 +1,14 @@
+var twitter = require("twitter");
+var spotify = require("spotify");
+var request = require("request");
+
+var keys = require("./keys.js");
+
+
+// `my-tweets`
+
+// `spotify-this-song`
+
+// `movie-this`
+
+// `do-what-it-says`
