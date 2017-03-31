b0bcfee1bae00227eba5bd5971ce776f645d5b85
Erin <eringlabe@gmail.com>
2017-03-31 10:20:49 -0700

    three functions working with errors

--- liri.js
+++ liri.js
@@ -44,18 +44,18 @@ function runTwitter() {
 	access_token_secret: twitterKeys.twitterKeys.access_token_secret
 	});
 
-	client.get('statuses/user_timeline.json', function(error, tweets, response) {
-		if(error) throw error;
+	client.get('statuses/user_timeline', function(error, tweets, response) {
 
-		if(tweets) {
+		if(!error && tweets) {
 			for (var i = 0; i < 10; i++) {
 
-			console.log(tweets[i].text);
-			console.log(tweets[i].created_at);
-			console.log(line);
+				console.log(i+1 + ": " + tweets[i].text);
+				console.log(tweets[i].created_at);
+				console.log(line);
 
 			};
 		};
+
 	});
 }
 
@@ -87,7 +87,7 @@ function runSpotify() {
 				console.log("Preview: " + JSON.stringify(data.tracks.items[0].preview_url));
 				console.log(line);
 			}
-			
+
 		});
 	} 
 }
@@ -99,23 +99,23 @@ function runRequest() {
 	request("http://www.omdbapi.com/?t=" + media + "&y=&plot=short&r=json", function(error, response, body) {
 
 	// If the request is successful (i.e. if the response status code is 200)
-	if (!error && response.statusCode === 200) {
+		if (!error && response.statusCode === 200) {
 
-		// Parse the body of the site and recover just the imdbRating
-		// (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-		console.log(line);
-		console.log("Movie title: " + JSON.parse(body).Title);    
-		console.log("Release year: " + JSON.parse(body).Year);    
-		console.log("IMDB rating: " + JSON.parse(body).imdbRating);
-		console.log("Country: " + JSON.parse(body).Country);    
-		console.log("Language(s): " + JSON.parse(body).Language);    
-		console.log("Plot: " + JSON.parse(body).Plot);    
-		console.log("Actors: " + JSON.parse(body).Actors);  
-		console.log("Rotten Tomatoes rating: " + JSON.parse(body).Ratings[1].Value);    
+			// Parse the body of the site and recover just the imdbRating
+			// (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+			console.log(line);
+			console.log("Movie title: " + JSON.parse(body).Title);    
+			console.log("Release year: " + JSON.parse(body).Year);    
+			console.log("IMDB rating: " + JSON.parse(body).imdbRating);
+			console.log("Country: " + JSON.parse(body).Country);    
+			console.log("Language(s): " + JSON.parse(body).Language);    
+			console.log("Plot: " + JSON.parse(body).Plot);    
+			console.log("Actors: " + JSON.parse(body).Actors);  
+			console.log("Rotten Tomatoes rating: " + JSON.parse(body).Ratings[1].Value);    
 
-		// console.log("Rotten Tomatoes URL: " + JSON.parse(body).Actors);    
+			// console.log("Rotten Tomatoes URL: " + JSON.parse(body).Actors);    
 
-		console.log(line);
+			console.log(line);
 		}
 	});
 }
